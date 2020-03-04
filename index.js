729a53c89e4ce7e09f849139eaca6d2938c12dde
Terrie's Desk <tsw659@gmail.com>
2020-03-04 11:50:24 -0500

    Adding images and sound

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var drumNumber = document.querySelectorAll(".drum").length;
 for (var i = 0; i<drumNumber; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("I got clicked!");
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+
+
   });
 
 
